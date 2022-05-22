45916ab3ede9c66eeb3eb635d470780662f65c46
Sagir Hossain <sagir.hossain.524@gmail.com>
2022-05-22 13:28:24 +0600

    survey index api done

--- app/Controllers/Http/SurveysController.ts
+++ app/Controllers/Http/SurveysController.ts
@@ -1,7 +1,22 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { ModelPaginatorContract } from '@ioc:Adonis/Lucid/Orm'
+import { SurveySortOptions } from 'App/Enums/SurveySortOptions'
+import { SurveyStatuses } from 'App/Enums/SurveyStatuses'
+import Survey from 'App/Models/Survey'
+import SurveyListRequestValidator from './../../Validators/SurveyListRequestValidator'
+import { SurveyService } from './../../Services/SurveyService';
 
 export default class SurveysController {
-  public async index({}: HttpContextContract) {}
+  public async index({ request }: HttpContextContract): Promise<ModelPaginatorContract<Survey>> {
+    await request.validate(SurveyListRequestValidator)
+    return SurveyService.getList(
+      Number(request.input('page', 1)),
+      Number(request.input('perPage', 10)),
+      request.input('search', ''),
+      request.input('sortBy', SurveySortOptions.Latest),
+      request.input('status', SurveyStatuses.All)
+    )
+  }
 
   public async store({}: HttpContextContract) {}
 
---
+++ app/Services/SurveyService.ts
@@ -0,0 +1,68 @@
+import { SurveySortOptions } from 'App/Enums/SurveySortOptions'
+import { SurveyStatuses } from 'App/Enums/SurveyStatuses'
+import Survey from 'App/Models/Survey'
+import { DateTime } from 'luxon'
+
+export class SurveyService {
+  public static getList(
+    page: number,
+    perPage: number,
+    search: string,
+    sortBy: SurveySortOptions,
+    status: SurveyStatuses
+  ) {
+    const query = Survey.query().withCount('questions')
+    const nowSql = DateTime.now().toSQL()
+
+    switch (status) {
+      case SurveyStatuses.Finished:
+        query.whereNotNull('ends_at').andWhere('ends_at', '<=', nowSql)
+        break
+      case SurveyStatuses.Published:
+        query.where('publish_at', '>=', nowSql)
+        break
+      case SurveyStatuses.Active:
+        query.where('publish_at', '>=', nowSql).andWhere((q) => {
+          q.whereNull('ends_at').orWhere('ends_at', '>', nowSql)
+        })
+        break
+      case SurveyStatuses.Unpublished:
+        query.where('publish_at', '<', nowSql)
+        break
+    }
+
+    if (search) {
+      query.where((q) => {
+        q.where('title', 'like', `%${search}%`).orWhere('description', 'like', `%${search}%`)
+      })
+    }
+
+    let sortByColumn: string
+    let sortByDirection: 'asc' | 'desc'
+
+    switch (sortBy) {
+      case SurveySortOptions.Latest:
+        sortByColumn = 'publish_at'
+        sortByDirection = 'desc'
+        break
+      case SurveySortOptions.Oldest:
+        sortByColumn = 'publish_at'
+        sortByDirection = 'asc'
+        break
+      case SurveySortOptions.Longest:
+        sortByColumn = 'time_limit'
+        sortByDirection = 'desc'
+        break
+      case SurveySortOptions.Shortest:
+        sortByColumn = 'time_limit'
+        sortByDirection = 'asc'
+        break
+      case SurveySortOptions.Title:
+        sortByColumn = 'title'
+        sortByDirection = 'asc'
+        break
+    }
+
+    return query.orderBy(sortByColumn, sortByDirection).paginate(page, perPage)
+  }
+}

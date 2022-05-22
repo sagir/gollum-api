fcc963271aecf48fc68b11c640b258847e47ee91
Sagir Hossain <sagir.hossain.524@gmail.com>
2022-05-22 21:41:50 +0600

    ...

--- app/Controllers/Http/QuestionsController.ts
+++ app/Controllers/Http/QuestionsController.ts
@@ -1,8 +1,6 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class QuestionsController {
-  public async index({}: HttpContextContract) {}
-
   public async store({}: HttpContextContract) {}
 
   public async update({}: HttpContextContract) {}

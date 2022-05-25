ce7f87790313915e0143ed8773aa57691d727e9e
Sagir Hossain <sagir.hossain.524@gmail.com>
2022-05-25 23:57:47 +0600

    token response updated

--- app/Responses/TokenResponse.ts
+++ app/Responses/TokenResponse.ts
@@ -1,8 +1,4 @@
-import { OpaqueTokenContract } from '@ioc:Adonis/Addons/Auth'
-import RefreshToken from 'App/Models/RefreshToken'
-import User from 'App/Models/User'
-
 export interface TokenResponse {
-  token: OpaqueTokenContract<User>
-  refreshToken: RefreshToken
+  token: string
+  refreshToken: string
 }
